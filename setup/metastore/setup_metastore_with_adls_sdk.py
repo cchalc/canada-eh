4071cf5848465423fc81bc8513ba7d9d54205e5e
cchalc <cchalc>
2023-10-03 04:58:25 +0000

    update

--- setup/metastore/setup_metastore_with_adls_sdk.py
+++ setup/metastore/setup_metastore_with_adls_sdk.py
@@ -12,6 +12,14 @@ dbutils.widgets.text("domain_name", '')
 
 # COMMAND ----------
 
+# MAGIC %pip install azure-storage-file-datalake azure-identity
+
+# COMMAND ----------
+
+dbutils.library.restartPython()
+
+# COMMAND ----------
+
 # MAGIC %sql USE CATALOG hive_metastore
 
 # COMMAND ----------
@@ -51,6 +59,54 @@ dbutils.fs.ls(f"{adls_path}/{company_name}/{domain_name}")
 
 # COMMAND ----------
 
+# Try with ADLS client
+import os
+import random
+from azure.storage.filedatalake import (
+    DataLakeServiceClient,
+    DataLakeDirectoryClient,
+    FileSystemClient
+)
+from azure.identity import DefaultAzureCredential
+
+# COMMAND ----------
+
+def get_service_client_account_key(account_name, account_key) -> DataLakeServiceClient:
+    account_url = f"https://{account_name}.dfs.core.windows.net"
+    service_client = DataLakeServiceClient(account_url, credential=account_key)
+
+    return service_client
+
+# COMMAND ----------
+
+# Get datalake service client
+datalake_service_client = get_service_client_account_key(
+    account_name=storage_account_name, account_key=storage_account_key
+)
+file_system_client = datalake_service_client.get_file_system_client(container_name)
+directory_client = datalake_service_client.get_directory_client(file_system_client.file_system_name, domain_name)
+
+# COMMAND ----------
+
+# Get container names
+file_systems = datalake_service_client.list_file_systems()
+for file_system in file_systems:
+    print(file_system.name)
+
+# COMMAND ----------
+
+# find all sources
+directory_name = f"{company_name}/{domain_name}"
+source_paths = []
+paths = file_system_client.get_paths(path=directory_name)
+for path in paths:
+    if path.is_directory and "_delta_log" not in path.name:
+        source_paths.append(path.name)
+        unique_sources = list(set(filter(lambda x: x.count('/') == 2, source_paths)))
+        sources = [s.split('/')[-1] for s in unique_sources]
+
+# COMMAND ----------
+
 file_info = dbutils.fs.ls(f"{adls_path}/{company_name}/{domain_name}")
 last_dirs = []
 for file in file_info:
@@ -62,7 +118,17 @@ print(last_dirs)
 
 # COMMAND ----------
 
+# Find all tables
+directory_name = f"{company_name}/{domain_name}"
+tables = []
+paths = file_system_client.get_paths(path=directory_name)
+for path in paths:
+    if path.is_directory and "_delta_log" not in path.name:
+        tables.append(path.name)
 
+# COMMAND ----------
+
+print(tables)
 
 # COMMAND ----------
 

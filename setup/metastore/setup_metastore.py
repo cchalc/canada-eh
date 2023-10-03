fd988bec992990c0cc67a010ada0288b6a72975b
cchalc <cchalc>
2023-10-03 06:49:04 +0000

    a few updates

--- setup/metastore/setup_metastore.py
+++ setup/metastore/setup_metastore.py
@@ -1,10 +1,10 @@
 # Databricks notebook source
-# %sql DROP SCHEMA IF EXISTS domain1 CASCADE;
+# %sql DROP SCHEMA IF EXISTS source2 CASCADE;
 
 # COMMAND ----------
 
 # setup widgets
-dbutils.widgets.text("domain_name", '')
+dbutils.widgets.text("domain", '')
 
 # COMMAND ----------
 
@@ -16,18 +16,16 @@ dbutils.widgets.text("domain_name", '')
 
 # COMMAND ----------
 
-domain_name = dbutils.widgets.get("domain_name")
+domain = dbutils.widgets.get("domain")
 
 # COMMAND ----------
 
 storage_account_name = "canadaehstorage"
-container_name = "cjc"
-company_name = "company1"
-# domain_name = "domain1" # using widget instead
+container = "cjc"
+company = "company1"
 tenant_id = "9f37a392-f0ae-4280-9796-f1864a10effc" # Microsoft Entra ID
-# database_name = f"{domain_name}"
-adls_path = f"abfss://{container_name}@{storage_account_name}.dfs.core.windows.net"
-domain_path = f'{adls_path}/{company_name}/{domain_name}'
+adls_path = f"abfss://{container}@{storage_account_name}.dfs.core.windows.net"
+domain_path = f'{adls_path}/{company}/{domain}'
 
 # COMMAND ----------
 
@@ -53,23 +51,26 @@ def get_last_dir(file_path):
 
 # COMMAND ----------
 
-def create_external_tables(domain_path, source_name):
-    tables = get_last_dir(f"{domain_path}/{source_name}")
-    for table_name in tables:
+def create_external_tables(domain_path, source, db):
+    tables = get_last_dir(f"{domain_path}/{source}")
+    for table in tables:
         table_script = f"""
-        CREATE TABLE IF NOT EXISTS {source_name}.{table_name}
+        CREATE TABLE IF NOT EXISTS {db}.{table}
         USING DELTA
-        LOCATION '{domain_path}/{source_name}/{table_name}'
+        LOCATION '{domain_path}/{source}/{table}'
         """
         spark.sql(table_script)
 
 # COMMAND ----------
 
-sources = get_last_dir(f"{adls_path}/{company_name}/{domain_name}")
-for source_name in sources:
-    spark.sql(f"CREATE DATABASE IF NOT EXISTS {source_name}")
-    create_external_tables(domain_path, source_name)
-
-# COMMAND ----------
-
-
+sources = get_last_dir(f"{adls_path}/{company}/{domain}")
+for source in sources:
+    dbname = spark.sql(f"show schemas in hive_metastore like '{source}'")
+    if dbname.take(1) == 0:
+        db = source
+        print("empty")
+    else:
+        db = f"{source}_1"
+        print("not empty")
+    spark.sql(f"CREATE DATABASE IF NOT EXISTS {db}")
+    create_external_tables(domain_path, source, db)

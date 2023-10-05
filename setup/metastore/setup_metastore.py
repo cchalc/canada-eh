b4dc1602e2596bc8c40831f2e60f2f7acd896e85
cchalc <cchalc>
2023-10-05 23:04:22 +0000

    added try except block

--- setup/metastore/setup_metastore.py
+++ setup/metastore/setup_metastore.py
@@ -17,7 +17,7 @@ dbutils.widgets.dropdown("run", 'false', ['true', 'false'])
 
 # COMMAND ----------
 
-domain = dbutils.widgets.get("domain")
+
 
 # COMMAND ----------
 
@@ -26,9 +26,11 @@ from delta.tables import *
 
 # COMMAND ----------
 
+# initialize variables
 storage_account_name = "canadaehstorage"
 container = "cjc"
 company = "company1"
+domain = dbutils.widgets.get("domain")
 tenant_id = "9f37a392-f0ae-4280-9796-f1864a10effc" # Microsoft Entra ID
 adls_path = f"abfss://{container}@{storage_account_name}.dfs.core.windows.net"
 domain_path = f'{adls_path}/{company}/{domain}'
@@ -61,12 +63,15 @@ def create_external_tables(domain_path, source, db):
     tables = get_last_dir(f"{domain_path}/{source}")
     for table in tables:
         if DeltaTable.isDeltaTable(spark, f"{domain_path}/{source}/{table}"):
-            table_script = f"""
-            CREATE EXTERNAL TABLE IF NOT EXISTS {db}.{table}
-            USING DELTA
-            LOCATION '{domain_path}/{source}/{table}'
-            """
-            spark.sql(table_script)
+            try:
+                table_script = f"""
+                CREATE EXTERNAL TABLE IF NOT EXISTS {db}.{table}
+                USING DELTA
+                LOCATION '{domain_path}/{source}/{table}'
+                """
+                spark.sql(table_script)
+            except Exception as e:
+                print(f"Error: {e}")
         else:
             print(f"{domain_path}/{source}/{table} is not a delta table")
 
@@ -75,26 +80,9 @@ def create_external_tables(domain_path, source, db):
 if dbutils.widgets.get("run") == "true":
     sources = get_last_dir(f"{adls_path}/{company}/{domain}")
     for source in sources:
+        db = source
         spark.sql(f"CREATE DATABASE IF NOT EXISTS {db}")
         create_external_tables(domain_path, source, db)
-
-# COMMAND ----------
-
-# explore try catch
-tables = get_last_dir(f"{domain_path}/{source}")
-for table in tables:
-    if DeltaTable.isDeltaTable(spark, f"{domain_path}/{source}/{table}"):
-        try:
-            table_script = f"""
-            CREATE EXTERNAL TABLE IF NOT EXISTS {db}.{table}
-            USING DELTA
-            LOCATION '{domain_path}/{source}/{table}'
-            """
-            print(source, table, table_script)
-        except Exception as e:
-            print(f"Error: {e}")
-    else:
-        print(f"{domain_path}/{source}/{table} is not a delta table")
 
 # COMMAND ----------
 
